6610bee7701019ffb96aa2a776a842a24089389d
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2018-12-06 13:18:13 -0800

    Update version number.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with io.open(os.path.join(maindir, 'README.md'), encoding='utf-8') as file:
 
 # Configure setuptools.
 setuptools.setup(name='duallog',
-                 version='0.12',
+                 version='0.13',
                  description='Parallel logging to console and logfile',
                  long_description=readme,
                  long_description_content_type='text/markdown',
